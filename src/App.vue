47052baf6b5f02a1e913194af3abf9467000b248
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2017-03-09 23:00:23 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <template>
     <div>
+        <h1>vuex-demo</h1>
         <Display></Display>
         <Increment></Increment>
-        <Reduce></Reduce>
     </div>
 </template>
 
 <script>
     import Display from './Display.vue'
     import Increment from './Increment.vue'
-    import Reduce from './Reduce.vue'
     import store from './vuex/store'
 
     export default {
         components: {
             Display,
-            Increment,
-            Reduce
+            Increment
         },
         store
     }
